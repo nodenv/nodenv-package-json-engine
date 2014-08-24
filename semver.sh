7273295c103141cbcd012389633a984f8e4c5dac
Józef Sokołowski <j.k.sokolowski@gmail.com>
2014-08-25 00:20:28 +0200

    It works... more or less...

--- semver.sh
+++ semver.sh
@@ -24,9 +24,34 @@ get_labels()
 
 }
 
+get_major()
+{
+    echo "$1" | grep -E -o "^$_num_part"
+}
+
+get_minor()
+{
+    min=$(echo "$1" | grep -E -o "^$_num_part.$_num_part")
+    min=${min#*.}
+
+    if [ -z "$min" ]; then
+        echo -n 0
+    else
+        echo -n $min
+    fi
+}
+
+strip_metadata()
+{
+    echo -n ${1%%+*}
+}
+
 semver_eq()
 {
-    [ "$1" = "$2" ] && return 0 || return 1
+    ver1=$(strip_metadata $1)
+    ver2=$(strip_metadata $2)
+
+    [ "$ver1" = "$ver2" ] && return 0 || return 1
 }
 
 semver_lt()
@@ -92,7 +117,7 @@ regex_match()
     local string="$1"
     local regexp="$2"
     local bre="$(eval "echo '$regexp' | sed 's/\\([(){}]\\)/\\\\\\1/g'")"
-    local match="$(eval "echo '$string' | grep --extended-regexp --line-regexp '$regexp'")";
+    local match="$(eval "echo '$string' | grep -E -x '$regexp'")";
 
     for i in $(seq 0 9); do
         unset "MATCHED_VER_$i"
@@ -107,9 +132,9 @@ regex_match()
     local part
     local i=1
     for part in $(echo $string); do
-        local ver="$(eval "echo '$part' | grep --extended-regexp --only-matching '$RE_VER'")";
-        local num="$(eval "echo '$part' | grep -E -o '$RE_NUM' | head -n1")";
-        local lab="$(eval "echo '$part' | grep -E -o '\-$RE_LAB' | cut -c 2-")";
+        local ver="$(eval "echo '$part' | grep -E -o '$RE_VER'   | head -n 1 ")";
+        local num="$(eval "echo '$part' | grep -E -o '$RE_NUM'   | head -n 1 ")";
+        local lab="$(eval "echo '$part' | grep -E -o '\-$RE_LAB' | cut -c 2- ")";
 
         if [ -n "$ver" ]; then
             eval "MATCHED_VER_$i='$ver'"
@@ -125,8 +150,8 @@ regex_match()
 resolve_rule()
 {
     # Specific version
-    if regex_match "$1" "$RE_VER"; then
-        echo "specific $MATCHED_VER_1"
+    if regex_match "$1" "[v=]?$RE_VER"; then
+        echo "eq $MATCHED_VER_1"
 
     # Greater than
     elif regex_match "$1" ">$RE_VER"; then
@@ -160,7 +185,7 @@ resolve_rule()
     elif regex_match "$1" "~$RE_VER"; then
         echo "tilde $MATCHED_VER_1"
     elif regex_match "$1" "$RE_NUM(\.[x*])+"; then
-        echo "tilde $MATCHED_VER_1"
+        echo "tilde $MATCHED_NUM_1"
 
     # Caret
     elif regex_match "$1" "\^$RE_VER"; then
@@ -172,3 +197,95 @@ resolve_rule()
 
     return 0
 }
+
+# Rules
+rule_eq()
+{
+    semver_eq $2 $1 && return 0 || return 1;
+}
+
+rule_le()
+{
+    semver_le $2 $1 && return 0 || return 1;
+}
+
+rule_lt()
+{
+    semver_lt $2 $1 && return 0 || return 1;
+}
+
+rule_ge()
+{
+    semver_ge $2 $1 && return 0 || return 1;
+}
+
+rule_gt()
+{
+    semver_gt $2 $1 && return 0 || return 1;
+}
+
+rule_gt_lt()
+{
+    if semver_gt $3 $1 && semver_lt $3 $2; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+rule_gt_le()
+{
+    if semver_gt $3 $1 && semver_le $3 $2; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+rule_ge_lt()
+{
+    if semver_ge $3 $1 && semver_lt $3 $2; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+rule_ge_le()
+{
+    if semver_ge $3 $1 && semver_le $3 $2; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+rule_tilde()
+{
+    ver1=$(get_version "$1")
+    maj2=$(get_major "$2")
+    min2=$(get_minor "$2")
+
+    if semver_ge "$3" "$maj2"".$min2"".0-0" && semver_le; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+while getopts r: o; do
+    case "$o" in
+        r) rule="rule_$(resolve_rule $OPTARG)";;
+    esac
+done
+
+shift $(( $OPTIND-1 ))
+
+for ver in $@; do
+    if [ -z "$rule" ] || $rule "$ver"; then
+        if semver_lt "$max" "$ver"; then
+            max="$ver"
+            echo $ver
+        fi
+    fi
+done
--- tests.sh
+++ tests.sh
@@ -62,6 +62,17 @@ describe 'get_labels'
     RET=$(get_labels 1.2.3.4-1.2.3.4.a.b.c-def)
     assert "$RET" "1.2.3.4.a.b.c-def"
 
+describe 'get_major'
+    RET=$(get_major 1.2.3.4)
+    assert "$RET" "1"
+
+describe 'get_minor'
+    RET=$(get_minor 1.2.3.4)
+    assert "$RET" "2"
+
+    RET=$(get_minor 1)
+    assert "$RET" "0"
+
 describe 'semver_eq'
     semver_eq 1.2.3 1.2.3
     assert $? 0
@@ -110,69 +121,89 @@ describe 'semver_ge'
     assert $? 0
 
 describe 'regex_match'
-    regex_match "1.22.333 - 1.2.3-3.2.1-7.8.0-abc-def+1.2.3" "$RE_VER - $RE_VER"
-    assert $? 0                                                 "Exit code should be 0 when match"
-    assert "$MATCHED_VER_1" "1.22.333"                          "Should set MATCHED_VER_1"
-    assert "$MATCHED_VER_2" "1.2.3-3.2.1-7.8.0-abc-def+1.2.3"   "Should set MATCHED_VER_2"
-    assert "$MATCHED_NUM_1" "1.22.333"                          "Should set MATCHED_NUM_1"
-    assert "$MATCHED_NUM_2" "1.2.3"                             "Should set MATCHED_NUM_2"
-    assert "$MATCHED_LAB_1" ""                                  "Should set MATCHED_LAB_1"
-    assert "$MATCHED_LAB_2" "3.2.1-7.8.0-abc-def"               "Should set MATCHED_LAB_2"
+    regex_match "1.22.333 - 1.2.3-3.2.1-a.b.c-def+011.a.1" "$RE_VER - $RE_VER"
+    assert $? 0                                             "Exit code should be 0 when match"
+    assert "$MATCHED_VER_1" "1.22.333"                      "Should set MATCHED_VER_1"
+    assert "$MATCHED_VER_2" "1.2.3-3.2.1-a.b.c-def+011.a.1" "Should set MATCHED_VER_2"
+    assert "$MATCHED_NUM_1" "1.22.333"                      "Should set MATCHED_NUM_1"
+    assert "$MATCHED_NUM_2" "1.2.3"                         "Should set MATCHED_NUM_2"
+    assert "$MATCHED_LAB_1" ""                              "Should set MATCHED_LAB_1"
+    assert "$MATCHED_LAB_2" "3.2.1-a.b.c-def"               "Should set MATCHED_LAB_2"
 
     regex_match '1.2.3 - 5.6.7' '5.6.7'
-    assert $? 1                                                 "Exit code should be 1 when don't match"
-    assert "$MATCHED_VER_1" ""                                  "When don't match MATCHED_VER_x should be empty"
-    assert "$MATCHED_VER_1" ""                                  "When don't match MATCHED_NUM_x should be empty"
-    assert "$MATCHED_LAB_1" ""                                  "When don't match MATCHED_LAB_x should be empty"
+    assert $? 1                                             "Exit code should be 1 when don't match"
+    assert "$MATCHED_VER_1" ""                              "When don't match MATCHED_VER_x should be empty"
+    assert "$MATCHED_VER_1" ""                              "When don't match MATCHED_NUM_x should be empty"
+    assert "$MATCHED_LAB_1" ""                              "When don't match MATCHED_LAB_x should be empty"
 
 describe 'reslove_rule'
-    RET=$(resolve_rule '1.2.3')
-    assert "$RET" "specific 1.2.3"          "Specific (1.2.3)"
+    RET=$(resolve_rule 'v1.2.3')
+    assert "$RET" "eq 1.2.3"                                "Specific (v1.2.3)"
 
     RET=$(resolve_rule '1')
-    assert "$RET" "specific 1"              "Specific (1)"
+    assert "$RET" "eq 1"                                    "Specific (1)"
 
-    RET=$(resolve_rule '1.2.3-a.2-c')
-    assert "$RET" "specific 1.2.3-a.2-c"    "Specific (1.2.3-a.2-c)"
+    RET=$(resolve_rule '=1.2.3-a.2-c')
+    assert "$RET" "eq 1.2.3-a.2-c"                          "Specific (=1.2.3-a.2-c)"
 
     RET=$(resolve_rule '>1.2.3')
-    assert "$RET" "gt 1.2.3"                "Greater than (>1.2.3)"
+    assert "$RET" "gt 1.2.3"                                "Greater than (>1.2.3)"
 
     RET=$(resolve_rule '<1.2.3')
-    assert "$RET" "lt 1.2.3"                "Less than (<1.2.3)"
+    assert "$RET" "lt 1.2.3"                                "Less than (<1.2.3)"
 
     RET=$(resolve_rule '>=1.2.3')
-    assert "$RET" "ge 1.2.3"                "Greater than or equal to (>=1.2.3)"
+    assert "$RET" "ge 1.2.3"                                "Greater than or equal to (>=1.2.3)"
 
     RET=$(resolve_rule '<=1.2.3')
-    assert "$RET" "le 1.2.3"                "Less than or equal to (<=1.2.3)"
+    assert "$RET" "le 1.2.3"                                "Less than or equal to (<=1.2.3)"
 
     RET=$(resolve_rule '1.2.3 - 4.5.6')
-    assert "$RET" "ge_le 1.2.3 4.5.6"       "Range (1.2.3 - 4.5.6)"
+    assert "$RET" "ge_le 1.2.3 4.5.6"                       "Range (1.2.3 - 4.5.6)"
 
     RET=$(resolve_rule '>1.2.3 <4.5.6')
-    assert "$RET" "gt_lt 1.2.3 4.5.6"       "Range (>1.2.3 <4.5.6)"
+    assert "$RET" "gt_lt 1.2.3 4.5.6"                       "Range (>1.2.3 <4.5.6)"
 
     RET=$(resolve_rule '>1.2.3 <=4.5.6')
-    assert "$RET" "gt_le 1.2.3 4.5.6"       "Range (>1.2.3 <=4.5.6)"
+    assert "$RET" "gt_le 1.2.3 4.5.6"                       "Range (>1.2.3 <=4.5.6)"
 
     RET=$(resolve_rule '>=1.2.3 <4.5.6')
-    assert "$RET" "ge_lt 1.2.3 4.5.6"       "Range (>=1.2.3 <4.5.6)"
+    assert "$RET" "ge_lt 1.2.3 4.5.6"                       "Range (>=1.2.3 <4.5.6)"
 
     RET=$(resolve_rule '>=1.2.3 <=4.5.6')
-    assert "$RET" "ge_le 1.2.3 4.5.6"       "Range (>=1.2.3 <=4.5.6)"
+    assert "$RET" "ge_le 1.2.3 4.5.6"                       "Range (>=1.2.3 <=4.5.6)"
 
     RET=$(resolve_rule '~1.2.3')
-    assert "$RET" "tilde 1.2.3"             "Tilde (~1.2.3)"
+    assert "$RET" "tilde 1.2.3"                             "Tilde (~1.2.3)"
 
     RET=$(resolve_rule '1.2.x')
-    assert "$RET" "tilde 1.2"               "Wildcard (1.2.x)"
+    assert "$RET" "tilde 1.2"                               "Wildcard (1.2.x)"
 
     RET=$(resolve_rule '1.*')
-    assert "$RET" "tilde 1"                 "Wildcard (1.*)"
+    assert "$RET" "tilde 1"                                 "Wildcard (1.*)"
 
     RET=$(resolve_rule '^1.2.3')
-    assert "$RET" "caret 1.2.3"             "Caret (^1.2.3)"
+    assert "$RET" "caret 1.2.3"                             "Caret (^1.2.3)"
+
+describe "rule_eq"
+    rule_eq '1.2.3' '1.2.3'
+    assert $? 0
+
+    rule_eq '1.2.3-abc+abc' '1.2.3-abc+xyz'
+    assert $? 0
+
+    rule_eq '1.2.3-a' '1.2.3-b'
+    assert $? 1
+
+describe "rule_tilde"
+    rule_tilde '1.2.3' '1.2.2'
+    assert $? 1
+
+    rule_tilde '1.2.3' '1.2.3-0'
+    assert $? 0
+
+    rule_tilde '1.2.3' '1.3.0-0'
+    assert $? 1
 
 
 # Summary
