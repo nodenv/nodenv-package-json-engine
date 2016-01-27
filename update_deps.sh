9027f357f7a60f9fe6a2aafd80274bd631eccc29
Jason Karns <jason@karns.name>
2016-01-26 21:14:50 -0500

    update deps via npm script

--- update_deps.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Run this at development-time to pull the latest copies
-# of dependencies from github.
-
-# Semver
-# https://github.com/qzb/sh-semver
-curl "https://raw.githubusercontent.com/qzb/sh-semver/master/semver.sh" > deps/semver.sh
