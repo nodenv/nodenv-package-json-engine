ad951db6bd13f701225ee30a2b85b1bda50fd37b
Adam Hull <adam@hmlad.com>
2018-02-24 23:44:58 -0800

    Lint with Shellcheck
    And run automatically on travis

--- etc/nodenv.d/version-name/package-json-engine.bash
+++ etc/nodenv.d/version-name/package-json-engine.bash
@@ -1,8 +1,11 @@
+#!/bin/bash
+
+# shellcheck source=libexec/nodenv-package-json-engine
 source "$(plugin_root)/libexec/nodenv-package-json-engine"
 
 if ! NODENV_PACKAGE_JSON_VERSION=$(get_version_respecting_precedence); then
   echo "package-json-engine: no version satisfying \`$(get_expression_respecting_precedence)' installed" >&2
   exit 1
 elif [ -n "$NODENV_PACKAGE_JSON_VERSION" ]; then
-  NODENV_VERSION="${NODENV_PACKAGE_JSON_VERSION}"
+  export NODENV_VERSION="${NODENV_PACKAGE_JSON_VERSION}"
 fi
--- etc/nodenv.d/version-origin/package-json-engine.bash
+++ etc/nodenv.d/version-origin/package-json-engine.bash
@@ -1,6 +1,9 @@
+#!/bin/bash
+
+# shellcheck source=libexec/nodenv-package-json-engine
 source "$(plugin_root)/libexec/nodenv-package-json-engine"
 
 ENGINES_EXPRESSION=$(get_expression_respecting_precedence);
 if [ -n "$ENGINES_EXPRESSION" ]; then
-  NODENV_VERSION_ORIGIN="package-json-engine matching $ENGINES_EXPRESSION"
+  export NODENV_VERSION_ORIGIN="package-json-engine matching $ENGINES_EXPRESSION"
 fi
