05dff41a6d8893102b34b666f2b1543652712bd9
Adam Hull <adam@hmlad.com>
2018-02-24 21:30:48 -0800

    Reorganize functions in libexec
    Prompted by the need to share precedence checks betweeen version-name
    and version-origin, I've extracted mosted of the package-json-engine
    logic into functions in libexec.
    
    Also fixes #10.

--- etc/nodenv.d/version-name/package-json-engine.bash
+++ etc/nodenv.d/version-name/package-json-engine.bash
@@ -1,5 +1,7 @@
-if ! NODENV_PACKAGE_JSON_VERSION=$(nodenv-package-json-engine); then
-  echo "package-json-engine: no version satisfying \`$NODENV_PACKAGE_JSON_VERSION' installed" >&2
+source "$(plugin_root)/libexec/nodenv-package-json-engine"
+
+if ! NODENV_PACKAGE_JSON_VERSION=$(get_version_respecting_precedence); then
+  echo "package-json-engine: no version satisfying \`$(get_expression_respecting_precedence)' installed" >&2
   exit 1
 elif [ -n "$NODENV_PACKAGE_JSON_VERSION" ]; then
   NODENV_VERSION="${NODENV_PACKAGE_JSON_VERSION}"
--- etc/nodenv.d/version-origin/package-json-engine.bash
+++ etc/nodenv.d/version-origin/package-json-engine.bash
@@ -1,4 +1,6 @@
-ENGINES_EXPRESSION=$(nodenv-package-json-extract-expression);
+source "$(plugin_root)/libexec/nodenv-package-json-engine"
+
+ENGINES_EXPRESSION=$(get_expression_respecting_precedence);
 if [ -n "$ENGINES_EXPRESSION" ]; then
   NODENV_VERSION_ORIGIN="package-json-engine matching $ENGINES_EXPRESSION"
 fi
