0f9efaafce101a62c1a70a9e90296ea787aa467f
Jason Karns <jason@karns.name>
2018-11-21 16:32:16 -0500

    Switch to ztombol's bats-assert

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -1,6 +1,7 @@
 # shellcheck shell=bash
 
-load ../node_modules/bats-assert/all
+load '../node_modules/bats-support/load'
+load '../node_modules/bats-assert/load'
 
 setup() {
   # common nodenv setup
