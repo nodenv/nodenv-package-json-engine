d6c01f60097385eb9dca90df3a2f5f776f6ca0c3
Jason Karns <jason@karns.name>
2019-01-20 16:17:23 -0500

    fixup! Rename nodenv-package-json

--- etc/nodenv.d/version-name/package-json-engine.bash
+++ etc/nodenv.d/version-name/package-json-engine.bash
@@ -3,7 +3,7 @@ if [ -n "$(nodenv-sh-shell 2>/dev/null)" ] ||
   return
 fi
 
-if NODENV_PACKAGE_JSON_VERSION=$(nodenv-package 2>/dev/null) &&
+if NODENV_PACKAGE_JSON_VERSION=$(nodenv-package-json 2>/dev/null) &&
   [ -n "$NODENV_PACKAGE_JSON_VERSION" ]; then
   export NODENV_VERSION=$NODENV_PACKAGE_JSON_VERSION
 fi
