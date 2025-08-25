324f99c38bb8c9e38a3ea6a47c013f8fef2b9f4b
Jason Karns <jason.karns@gmail.com>
2025-08-25 12:26:43 -0400

    shfmt --write

--- etc/nodenv.d/version-origin/package-json-engine.bash
+++ etc/nodenv.d/version-origin/package-json-engine.bash
@@ -1,7 +1,7 @@
 # shellcheck source=libexec/nodenv-package-json-engine
 source "$(plugin_root)/libexec/nodenv-package-json-engine"
 
-ENGINES_EXPRESSION=$(get_expression_respecting_precedence);
+ENGINES_EXPRESSION=$(get_expression_respecting_precedence)
 if [ -n "$ENGINES_EXPRESSION" ]; then
   # shellcheck disable=2034
   NODENV_VERSION_ORIGIN="$(package_json_path)#engines.node"
